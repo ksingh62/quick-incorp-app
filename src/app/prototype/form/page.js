005f2d9dc3b1341cdab61287908dea8073834d4c
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-05-28 13:49:27 -0600

    bug fix: website reroutes to landing page

--- src/app/prototype/form/page.js
+++ src/app/prototype/form/page.js
@@ -1,11 +1,15 @@
+'use client'
 import Form from "@/components/Form/form.js";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-    return (
-        <section className="py-24">
-            <div className="container">
-                <Form />
-            </div>
-        </section>
-    );
+  return (
+    // <Layout>
+      <section className="py-24">
+        <div className="container">
+          <Form />
+        </div>
+      </section>
+    // </Layout>
+  );
 }
