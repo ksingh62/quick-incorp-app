3b80f93311d8bd26902d37b5f661eec8abb7285c
aulakhsahib <sahibaulakh2003@gmail.com>
2024-06-02 16:07:52 -0600

    added personal details section

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,43 +1,67 @@
 import { useForm } from "react-hook-form";
 
-export default function App() {
+export default function Form() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    mode: "onChange"
+  });
+
   const onSubmit = (data) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {/* Personal Details */}
       <div>
-        <label htmlFor="first-name">First Name</label>
-        <input
-          {...register("firstName", {
-            required: { value: true, message: "First name is required" },
-            minLength: {
-              value: "5",
-              message: "First Name should at least of 5 characters",
-            },
-          })}
-          className="text-black"
-        />
-        <p className="text-red-600">
-          {errors.firstName && errors.firstName.message}
-        </p>
+        <label>
+          First Name
+          <input
+            className="text-black"
+            type="text"
+            {...register("firstName", {
+              required: {
+                value: "true",
+                message: "First Name is Required",
+              },
+              minLength: {
+                value: 4,
+                message: "First Name should at least be 4 characters long.",
+              },
+            })}
+          />
+        </label>
+        <p>{errors.firstName?.message}</p>
       </div>
 
       <div>
-        <label htmlFor="last-name">Last Name</label>
-        <input {...register("last-name")} className="text-black" />
+        <label>
+          Last Name
+          <input className="text-black" type="text" {...register("lastName")} />
+        </label>
       </div>
 
       <div>
-        <label htmlFor="phone-number">Phone Number</label>
-        <input {...register("phone-number")} className="text-black" />
+        <label>
+          Phone Number
+          <input
+            className="text-black"
+            type="tel"
+            {...register("phoneNumber")}
+          />
+        </label>
       </div>
 
       <button type="submit">Submit</button>
     </form>
   );
 }
+
+/* 
+Personal Details
+Corporation Details
+Business Address
+Plans
+Payment
+*/
