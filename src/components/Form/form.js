a7b685b5e355f03688abee28002a49f2babb9648
aulakhsahib <sahibaulakh2003@gmail.com>
2024-06-03 11:27:01 -0600

    removed mui

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,9 +1,4 @@
 import { useForm } from "react-hook-form";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
 import { useState } from "react";
 
 const steps = [
