27dd2342eadc3fc7738785404f81ef3dbcd7b60c
Ammar-Khan08 <130613802+Ammar-Khan08@users.noreply.github.com>
2024-06-03 15:36:24 -0600

    Added layout to Faq page

--- src/app/prototype/faq/page.js
+++ src/app/prototype/faq/page.js
@@ -1,11 +1,15 @@
+'use client'
 import React from 'react';
 import FAQ from './faq';
+import Layout from '@/components/Layout';
 
 const Page = () => {
   return (
+    <Layout>
     <div>
       <FAQ />
     </div>
+    </Layout>
   );
 };
 
