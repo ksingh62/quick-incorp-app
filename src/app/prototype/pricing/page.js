1003ae58aa0b5c3b57641a38c8eeb89c14931625
Vansh Malhotra <109238503+Vansh2727@users.noreply.github.com>
2024-06-03 15:04:37 -0600

    formed the pricing page and added layout.

--- src/app/prototype/pricing/page.js
+++ src/app/prototype/pricing/page.js
@@ -1,45 +1,28 @@
 "use client";
 import React from "react";
-import Layout from "../../../components/Layout";
 import "./pricing.css";
+import Layout from "@/components/Layout";
+import { useRouter } from "next/navigation";
 
-const PricingPage = () => {
+const Pricing = () => {
+  const router = useRouter();
+
+  const handleGetStarted = () => {
+    router.push("/prototype/form");
+  };
   return (
     <Layout>
-      <div className="pricing-container">
-        <h1>Pricing Plans</h1>
-        <div className="pricing-cards">
-          <div className="pricing-card">
-            <h2>Basic</h2>
-            <p>$70/month</p>
-            <ul>
-              <li>Company Name Registration</li>
-              <li>Filings with the Government</li>
-              <li>Up to 3 User Accounts</li>
-            </ul>
-            <a href="/prototype/pricing/basic-details.js">
-              <button>Choose Plan</button>
-            </a>
-          </div>
-          <div className="pricing-card">
-            <h2>Premium</h2>
-            <p>$100/month</p>
-            <ul>
-              <li>Full Incorporation plus additional features</li>
-              <li>Unlimited Access to QuickInCorp Legal Agreement Library</li>
-              <li>Unlimited User Accounts</li>
-              <li>Unlimited Company Updates & Government Filings</li>
-              <li>Unlimited Support</li>
-              <li>Unlimited User Accounts</li>
-            </ul>
-            <a href="/prototype/pricing/basic-details.js">
-              <button>Choose Plan</button>
-            </a>
-          </div>
-        </div>
+      <div className="container mx-auto text-center">
+        <h2 className="text-4xl font-semibold mb-6">
+          Fair and Transparent Pricing
+        </h2>
+        <p className="text-lg mb-12">
+          Whether you're starting a new business or already running one, we've
+          got you covered.
+        </p>
       </div>
     </Layout>
   );
 };
 
-export default PricingPage;
+export default Pricing;
