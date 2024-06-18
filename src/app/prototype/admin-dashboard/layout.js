bd732302339c719f992bed911ce89a91c4e3ef5b
aulakhsahib <sahibaulakh2003@gmail.com>
2024-06-18 10:55:46 -0600

    added calendar

--- src/app/prototype/admin-dashboard/layout.js
+++ src/app/prototype/admin-dashboard/layout.js
@@ -6,6 +6,9 @@ export default function AdminLayout({ children }) {
         <nav className="flex gap-6">
             <Link href="/prototype/admin-dashboard/" className="text-red-600">Dashboard Home</Link>
             <Link href="/prototype/admin-dashboard/manage-teams" className="text-red-600">Manage Teams</Link>
+            <Link href="/prototype/admin-dashboard/contacts" className="text-red-600">Contacts</Link>
+            <Link href="/prototype/admin-dashboard/invoices" className="text-red-600">Invoices</Link>
+            <Link href="/prototype/admin-dashboard/calendar" className="text-red-600">Calendar</Link>
         </nav>
         <div>{children}</div>
     </>
