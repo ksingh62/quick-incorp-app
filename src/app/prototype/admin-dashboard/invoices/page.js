40a0af4237b5d929bce3415425aad4c691532d49
aulakhsahib <sahibaulakh2003@gmail.com>
2024-06-19 15:01:19 -0600

    connected invoices page to database

--- src/app/prototype/admin-dashboard/invoices/page.js
+++ src/app/prototype/admin-dashboard/invoices/page.js
@@ -1,20 +1,37 @@
-import { mockDataInvoices} from "../mockData";
+"use client";
 import { DataGrid } from "@mui/x-data-grid";
-
+import { db } from "../../_utils/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
 export default function Page() {
-    const columns = [
-        { field: "id", headerName: "ID", headerClassName: "bg-purple-900"},
-        { field: "name", headerName: "Name", headerClassName: "bg-purple-900" },
-        { field: "email", headerName: "Email", headerClassName: "bg-purple-900"},
-        { field: "cost", headerName: "Cost", headerClassName: "bg-purple-900"},
-        { field: "phone", headerName: "Phone Number", headerClassName: "bg-purple-900"},
-        { field: "date", headerName: "Date", headerClassName: "bg-purple-900", flex: "1"},
-      ];
-    
-      return (
-        <>
-          <h1>Team</h1>
-          <DataGrid rows={mockDataInvoices}  columns={columns } sx={{color: "white"}} />
-        </>
-      );
+  const invoicesCollection = collection(db, "invoices");
+  const [invoices, setInvoices] = useState([]);
+
+  useEffect(() => {
+    const getInvoices = async () => {
+      const invoicesDocs = await getDocs(invoicesCollection);
+      const invoicesData = invoicesDocs.docs.map(doc => {
+        return { ...doc.data(), date: new Date(doc.data().date), id: doc.id }
+      })
+      setInvoices(invoicesData);
+    }
+    getInvoices();
+
+  }, [])
+
+
+  const columns = [
+    { field: 'invoiceId', headerName: 'Invoice ID', headerClassName: "bg-purple-900", flex: 1 },
+    { field: 'customerName', headerName: 'Customer Name', headerClassName: "bg-purple-900", flex: 1 },
+    { field: 'date', headerName: 'Date', headerClassName: "bg-purple-900", flex: 1, type: "date" },
+    { field: 'amount', headerName: 'Amount', headerClassName: "bg-purple-900", flex: 1, type: 'number' },
+    { field: 'status', headerName: 'Status', headerClassName: "bg-purple-900", flex: 1 },
+  ];
+
+  return (
+    <>
+      <h1>Team</h1>
+      <DataGrid rows={invoices} columns={columns} sx={{ color: "white" }} />
+    </>
+  );
 }
--- src/app/prototype/admin-dashboard/layout.js
+++ src/app/prototype/admin-dashboard/layout.js
@@ -11,6 +11,8 @@ export default function AdminLayout({ children }) {
                 <Link href="/prototype/admin-dashboard/contacts" className="text-red-600">Contacts</Link>
                 <Link href="/prototype/admin-dashboard/invoices" className="text-red-600">Invoices</Link>
                 <Link href="/prototype/admin-dashboard/calendar" className="text-red-600">Calendar</Link>
+                <Link href="/prototype/admin-dashboard/bar-chart" className="text-red-600">Bar Chart</Link>
+                <Link href="/prototype/admin-dashboard/line-chart" className="text-red-600">Line Chart</Link>
             </nav>
             <div>{children}</div>
         </EmployeeProvider>
