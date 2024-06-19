70557eb3bf9c69339d3a1cb5d4ddf4e2cc624f87
aulakhsahib <sahibaulakh2003@gmail.com>
2024-06-19 13:30:17 -0600

    added line chart, bar chart

---
+++ src/app/prototype/admin-dashboard/bar-chart/page.js
@@ -0,0 +1,12 @@
+import MyResponsiveBar from "@/components/BarChart/BarChart";
+
+export default function Page() {
+    return (
+        <>
+            <h1>Hello</h1>
+            <div style={{height: "75vh"}}>
+            <MyResponsiveBar />
+            </div>
+        </>
+    )
+}
---
+++ src/app/prototype/admin-dashboard/line-chart/page.js
@@ -0,0 +1,12 @@
+import MyResponsiveLine from "@/components/LineChart/LineChart";
+
+export default function Page() {
+    return (
+        <>
+            <h1>Hello</h1>
+            <div style={{ height: "75vh" }}>
+                <MyResponsiveLine />
+            </div>
+        </>
+    )
+}
