14fd16b0d4bca29deada4fa74fef55c9c2ffa90c
aulakhsahib <sahibaulakh2003@gmail.com>
2024-06-19 18:52:47 -0600

    added invoices provider

--- src/app/prototype/admin-dashboard/invoices/page.js
+++ src/app/prototype/admin-dashboard/invoices/page.js
@@ -1,47 +1,54 @@
 "use client";
 import { DataGrid } from "@mui/x-data-grid";
-import { db } from "../../_utils/firebase";
-import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
 import { useState, useEffect, useRef } from "react";
 import AddInvoiceModal from "@/components/AddInvoiceModal/AddInvoiceModal";
 import UpdateInvoiceModal from "@/components/UpdateInvoiceModal/UpdateInvoiceModal";
-export default function Page() {
-  const invoicesCollection = collection(db, "invoices");
-  const [invoices, setInvoices] = useState([]);
+import useInvoices from "@/hooks/useInvoices";
 
+export default function Page() {
   const [showAddInvoiceModal, setShowAddInvoiceModal] = useState(false);
   const [showUpdateInvoiceModal, setShowUpdateInvoiceModal] = useState(false);
   const invoiceToUpdate = useRef({});
-  const [noOfUsersAdded, setNoOfUsersAdded] = useState(0);
+  const {
+    invoices,
+    noOfUsersAdded,
+    getInvoices,
+    addInvoice,
+    updateInvoice,
+    deleteInvoice
+  } = useInvoices();
 
-  const getInvoices = async () => {
-    const invoicesDocs = await getDocs(invoicesCollection);
-    const invoicesData = invoicesDocs.docs.map(doc => {
-      return { ...doc.data(), date: new Date(doc.data().date), id: doc.id }
-    })
-    setInvoices(invoicesData);
-  }
+  // const invoicesCollection = collection(db, "invoices");
+  // const [invoices, setInvoices] = useState([]);
+  // const [noOfUsersAdded, setNoOfUsersAdded] = useState(0);
+  // const getInvoices = async () => {
+  //   const invoicesDocs = await getDocs(invoicesCollection);
+  //   const invoicesData = invoicesDocs.docs.map(doc => {
+  //     return { ...doc.data(), date: new Date(doc.data().date), id: doc.id }
+  //   })
+  //   setInvoices(invoicesData);
+  // }
 
-  const addInvoice = async (invoiceToAdd) => {
-    await addDoc(invoicesCollection, invoiceToAdd);
-    setNoOfUsersAdded(noOfUsersAdded + 1);
-  }
+  // const addInvoice = async (invoiceToAdd) => {
+  //   await addDoc(invoicesCollection, invoiceToAdd);
+  //   setNoOfUsersAdded(noOfUsersAdded + 1);
+  // }
 
-  const updateInvoice = async (invoiceDocId, newInvoiceInfo) => {
-    const invoice = doc(db, "invoices", invoiceDocId);
-    const res = await updateDoc(invoice, newInvoiceInfo);
-    setNoOfUsersAdded(noOfUsersAdded + 1);
-  }
+  // const updateInvoice = async (invoiceDocId, newInvoiceInfo) => {
+  //   const invoice = doc(db, "invoices", invoiceDocId);
+  //   const res = await updateDoc(invoice, newInvoiceInfo);
+  //   setNoOfUsersAdded(noOfUsersAdded + 1);
+  // }
 
-  const deleteInvoice = async (invoiceDocId) => {
-    const invoice = doc(db, "invoices", invoiceDocId);
-    await deleteDoc(invoice);
-    setNoOfUsersAdded(noOfUsersAdded - 1);
-  }
+  // const deleteInvoice = async (invoiceDocId) => {
+  //   const invoice = doc(db, "invoices", invoiceDocId);
+  //   await deleteDoc(invoice);
+  //   setNoOfUsersAdded(noOfUsersAdded - 1);
+  // }
 
   useEffect(() => {
     getInvoices();
-  }, [noOfUsersAdded])
+  }, [getInvoices, noOfUsersAdded])
 
   const onDeleteClick = async (event, params) => {
     deleteInvoice(params.row.id);
--- src/app/prototype/admin-dashboard/layout.js
+++ src/app/prototype/admin-dashboard/layout.js
@@ -1,21 +1,24 @@
 import { EmployeeProvider } from "@/context/EmployeeProvider";
+import { InvoicesProvider } from "@/context/InvoicesProvider";
 import Link from "next/link";
 
 export default function AdminLayout({ children }) {
     return (
         <EmployeeProvider>
-            <h2>Sidebar</h2>
-            <nav className="flex gap-6">
-                <Link href="/prototype/admin-dashboard/" className="text-red-600">Dashboard Home</Link>
-                <Link href="/prototype/admin-dashboard/manage-teams" className="text-red-600">Manage Teams</Link>
-                <Link href="/prototype/admin-dashboard/contacts" className="text-red-600">Contacts</Link>
-                <Link href="/prototype/admin-dashboard/invoices" className="text-red-600">Invoices</Link>
-                <Link href="/prototype/admin-dashboard/calendar" className="text-red-600">Calendar</Link>
-                <Link href="/prototype/admin-dashboard/bar-chart" className="text-red-600">Bar Chart</Link>
-                <Link href="/prototype/admin-dashboard/line-chart" className="text-red-600">Line Chart</Link>
-                <Link href="/prototype/admin-dashboard/pie-chart" className="text-red-600">Pie Chart</Link>
-            </nav>
-            <div>{children}</div>
+            <InvoicesProvider>
+                <h2>Sidebar</h2>
+                <nav className="flex gap-6">
+                    <Link href="/prototype/admin-dashboard/" className="text-red-600">Dashboard Home</Link>
+                    <Link href="/prototype/admin-dashboard/manage-teams" className="text-red-600">Manage Teams</Link>
+                    <Link href="/prototype/admin-dashboard/contacts" className="text-red-600">Contacts</Link>
+                    <Link href="/prototype/admin-dashboard/invoices" className="text-red-600">Invoices</Link>
+                    <Link href="/prototype/admin-dashboard/calendar" className="text-red-600">Calendar</Link>
+                    <Link href="/prototype/admin-dashboard/bar-chart" className="text-red-600">Bar Chart</Link>
+                    <Link href="/prototype/admin-dashboard/line-chart" className="text-red-600">Line Chart</Link>
+                    <Link href="/prototype/admin-dashboard/pie-chart" className="text-red-600">Pie Chart</Link>
+                </nav>
+                <div>{children}</div>
+            </InvoicesProvider>
         </EmployeeProvider>
     )
 }
