bece9e4753c3601b755f8775ec3d242702ad0eb4
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-06-19 19:27:23 -0600

    change admin email

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -68,7 +68,7 @@ export default async function handler(req, res) {
 
     const mailOptionsLawyer = {
       from: "no-reply@yourdomain.com",
-      to: "kapilmeetbaath@gmail.com",
+      to: "quickincorp51@gmail.com",
       subject: `New Business Registration - Application ID: ${applicationId}`,
       html: emailTemplate(
         "New Business Registration",
