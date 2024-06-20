de1a4f452cc697e61f79e8bcd384b094366e1e0d
Ammar-Khan08 <130613802+Ammar-Khan08@users.noreply.github.com>
2024-06-20 09:31:38 -0600

    Added hidesidebar and showsidebar buttons.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,58 +1,72 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
-import "./Sidebar.css";
+import { useState } from "react";
 import Link from "next/link";
+import "./Sidebar.css";
 
 export default function Sidebar() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="sidebar-logo-options-container">
-        <Link href={`/prototype/homepage`} className="sidebar-logo-container">
-          <img src="/logo-mobile.svg" alt="" />
-          <p>QuickIncorp</p>
-        </Link>
+    <>
+      {isSidebarVisible ? (
+        <div className="sidebar">
+          <div className="sidebar-logo-options-container">
+            <Link href={`/prototype/homepage`} className="sidebar-logo-container">
+              <img src="/logo-mobile.svg" alt="" />
+              <p>QuickIncorp</p>
+            </Link>
 
-        <div className="sidebar-options">
-          <div className="sidebar-option ">
-            <img src="/icon-board.svg" alt="" />
-            <Link href='/prototype/form'>Register a Company</Link>
+            <div className="sidebar-options">
+              <div className="sidebar-option ">
+                <img src="/icon-board.svg" alt="" />
+                <Link href='/prototype/form'>Register a Company</Link>
+              </div>
+
+              <div className="sidebar-option active-sidebar-option">
+                <img src="/icon-board.svg" alt="" />
+                <Link href='/prototype/homepage'>Book a Session with advisor</Link>
+              </div>
+
+              <div className="sidebar-option">
+                <img src="/icon-board.svg" alt="" />
+                <Link href='/prototype/tax'>Tax Management</Link>
+              </div>
+
+              <div className="sidebar-option">
+                <img src="/icon-board.svg" alt="" />
+                <Link href='/prototype/inbox'>Inbox</Link>
+              </div>
+            </div>
           </div>
+          <div className="sidebar-help-hide-container">
+            <div className="sidebar-help-container">
+              <div className="sidebar-option">
+                <img src="/icon-board.svg" alt="" />
+                <p>Help & Support</p>
+              </div>
 
-          <div className="sidebar-option active-sidebar-option">
-            <img src="/icon-board.svg" alt="" />
-            <Link href='/prototype/homepage'>Book a Session with advisor</Link>
-          </div>
-
-          <div className="sidebar-option">
-            <img src="/icon-board.svg" alt="" />
-            <Link href='/prototype/tax'>Tax Management</Link>
-          </div>
-
-          <div className="sidebar-option">
-            <img src="/icon-board.svg" alt="" />
-            <Link href='/prototype/inbox'>Inbox</Link>
+              <div className="sidebar-option">
+                <img src="/icon-board.svg" alt="" />
+                <p>Settings</p>
+              </div>
+            </div>
+            <div className="hide-container">
+              <div className="sidebar-option" onClick={toggleSidebar}>
+                <img src="/icon-hide-sidebar.svg" alt="" />
+                <p>Hide Sidebar</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="sidebar-help-hide-container">
-        <div className="sidebar-help-container">
-          <div className="sidebar-option">
-            <img src="/icon-board.svg" alt="" />
-            <p>Help & Support</p>
-          </div>
-
-          <div className="sidebar-option">
-            <img src="/icon-board.svg" alt="" />
-            <p>Settings</p>
-          </div>
+      ) : (
+        <div className="show-sidebar-button" onClick={toggleSidebar}>
+          <img src="/icon-show-sidebar.svg" alt="Show Sidebar" />
         </div>
-        <div className="hide-container">
-          <div className="sidebar-option">
-            <img src="/icon-hide-sidebar.svg" alt="" />
-            <p>Hide Sidebar</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
