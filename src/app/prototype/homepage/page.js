f24390d1e229d55b4f87a901c050a9004c2f37c1
Ammar-Khan08 <130613802+Ammar-Khan08@users.noreply.github.com>
2024-07-08 20:03:45 -0600

    edited homepage

--- src/app/prototype/homepage/page.js
+++ src/app/prototype/homepage/page.js
@@ -1,16 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
-import "./page.css";
-import Navbar from "@/components/Navbar/Navbar";
-import Sidebar from "@/components/Sidebar/Sidebar";
-
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import Layout from "@/components/Layout";
+import React from "react";
 import { useUserAuth } from "../_utils/auth-context";
 import { useEffect } from "react";
-import Layout from "@/components/Layout";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
+function Page() {
   const { user } = useUserAuth();
 
   const router = useRouter();
@@ -23,81 +18,11 @@ export default function Home() {
     }
     // If there is a user, stay on this page or manage other routes as needed
   }, [user, router]);
-
   return (
     <Layout>
-      <div className="home-page">
-        {/* <Sidebar /> */}
-        <div>
-          {/* <Navbar /> */}
-
-          <div className="advisor-parent-container">
-            <div className="advisor-container">
-              <h2 className="advisor-container-heading">Financial Advisors</h2>
-
-              <div className="advisor-card-container">
-                <Link href={`/prototype/advisor`} className="advisor-card">
-                  <img src="/pic-2.jpg" alt="" />
-                  <p>Keith Piper</p>
-                </Link>
-
-                <div className="advisor-card">
-                  <Link href={`/prototype/advisor/a`} className="advisor-card">
-                    <img src="/pic-3.jpg" alt="" />
-                    <p>Andrew Scott</p>
-                  </Link>
-                </div>
-
-                <div className="advisor-card">
-                  <Link href={`/prototype/advisor/b`} className="advisor-card">
-                    <img src="/pic-4.jpg" alt="" />
-                    <p>Abigail Nolan</p>
-                  </Link>
-                </div>
-
-                <div className="advisor-card">
-                  <Link href={`/prototype/advisor/c`} className="advisor-card">
-                    <img src="/pic-1.jpg" alt="" />
-                    <p>Faith Morris</p>
-                  </Link>
-                </div>
-              </div>
-            </div>
-
-            <div className="advisor-container">
-              <h2 className="advisor-container-heading">Legal Advisors</h2>
-
-              <div className="advisor-card-container">
-              <Link href={`/prototype/advisor`} className="advisor-card">
-                  <img src="/pic-2.jpg" alt="" />
-                  <p>Keith Piper</p>
-                </Link>
-
-                <div className="advisor-card">
-                  <Link href={`/prototype/advisor/a`} className="advisor-card">
-                    <img src="/pic-3.jpg" alt="" />
-                    <p>Andrew Scott</p>
-                  </Link>
-                </div>
-
-                <div className="advisor-card">
-                  <Link href={`/prototype/advisor/b`} className="advisor-card">
-                    <img src="/pic-4.jpg" alt="" />
-                    <p>Abigail Nolan</p>
-                  </Link>
-                </div>
-
-                <div className="advisor-card">
-                  <Link href={`/prototype/advisor/c`} className="advisor-card">
-                    <img src="/pic-1.jpg" alt="" />
-                    <p>Faith Morris</p>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div>Home page</div>
     </Layout>
   );
 }
+
+export default Page;
