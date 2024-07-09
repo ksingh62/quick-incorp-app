379ec5a421d96b3c2b697f67c5a186f8668fba63
aulakhsahib <sahibaulakh2003@gmail.com>
2024-07-08 20:46:16 -0600

    added kanban board to dashboard

--- src/app/prototype/admin-dashboard/layout.js
+++ src/app/prototype/admin-dashboard/layout.js
@@ -13,6 +13,9 @@ export default function AdminLayout({ children }) {
                             <Link href="/prototype/admin-dashboard/" className="text-white hover:text-indigo-300">
                                 Dashboard Home
                             </Link>
+                            <Link href="/prototype/admin-dashboard/kanban" className="text-white hover:text-indigo-300">
+                                Kanban Board
+                            </Link>
                             <Link href="/prototype/admin-dashboard/manage-teams" className="text-white hover:text-indigo-300">
                                 Manage Teams
                             </Link>
